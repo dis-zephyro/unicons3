71764dd3dc1569cef6cfa9ca2aa9af7d036c5143
dis-zephyro <zephyro@yandex.ru>
2015-06-07 23:16:56 +0300

    v3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// подсказки для форм (можноу удалить)
+
+$(function(){
+    var mr_input="input[type=text]";
+    $(mr_input).each(function() {    $(this).data('stext',$(this).val());                                               });
+    $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
+    $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
+});
+
+
 $(document).ready(function() {
 
     $(window).bind('load', function() {
@@ -30,7 +40,28 @@ $(document).ready(function() {
 
     }
 
-    $(".popup, .certificate-img").fancybox({
+    $(".popup").fancybox({
         padding: '0'
     });
+
+    $(".reply__link").fancybox({
+        padding: '0'
+    });
+});
+
+// Слайдер с отзывами
+
+$('.reply__item').slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+});
+
+$('.reply__nav.prev').click(function(){
+    $('.reply__item').slick('slickPrev');
+});
+
+$('.reply__nav.next').click(function(){
+    $('.reply__item').slick('slickNext');
 });
